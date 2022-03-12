0b54192d50fb41939c9eff392b56ab261203734d
chenbang <1125272509@qq.com>
2022-03-12 17:05:00 +0800

    函数练习

---
+++ fun_test.py
@@ -0,0 +1,37 @@
+
+# def func (a,b,c):
+#     print('a=%s' %a)
+#     print('b=%s' %b)
+#     print('c=%s' %c)
+#
+# func(c=1,b=3,a=8)
+
+
+# 取函数的个数
+# def howlong(first,*others):
+#     print(1+len(others))
+#
+# howlong(1,3,4,5,6)
+
+
+# list1 = [1, 2, 3]
+# it = iter(list1)
+# print(next(it))
+# print(next(it))
+
+
+# for i in range(10,20,2):
+#     print(i)
+
+
+# def frange(start, stop, step):
+#     x = start
+#     while x < stop:
+#         yield x
+#         x += step
+#
+#
+#
+# for i in frange(10, 20, 0.5):
+#     print(i)
+
