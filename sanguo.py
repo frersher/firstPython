91df7521d60716e1ced33b34fca818f18026b22e
chenbang <1125272509@qq.com>
2022-03-12 11:27:09 +0800

    函数练习

--- sanguo.py
+++ sanguo.py
@@ -1,3 +1,21 @@
-f = open('name.text')
+# 读取任务名称
+f = open('name.txt')
 data = f.read()
 print(data.split('|'))
+
+# 读取兵器
+f2 = open('weapon.txt')
+i = 1
+for line in f2.readlines():
+    if i % 2 == 1:
+        print(line.strip('\n'))
+    i += 1
+
+f3 = open('sanguo.txt')
+print(f3.read().replace('\n',''))
+
+# 定义函数
+def func(filename):
+    print(open(filename).read())
+
+
---
+++ sanguo_v2.py
@@ -0,0 +1,17 @@
+# 统计任务在三国中出现的次数
+import re
+def count_name(name):
+    with open('sanguo.txt') as f:
+        data = f.read().replace('\n','')
+        name_num = re.findall(name,data)
+        print('%s 出现了 %s 次' %(name,len(name_num)))
+
+
+
+with open('name.txt') as f:
+    for line in f:
+       names = line.split('|')
+       for n in names:
+           count_name(n)
+
+
