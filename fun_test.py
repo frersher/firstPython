9cbdf3f9eeaeaf146e75dcb97dc4a4aa07b61584
chenbang <1125272509@qq.com>
2022-03-14 23:19:23 +0800

    函数练习

--- fun_test.py
+++ fun_test.py
@@ -1,4 +1,3 @@
-
 # def func (a,b,c):
 #     print('a=%s' %a)
 #     print('b=%s' %b)
@@ -35,3 +34,60 @@
 # for i in frange(10, 20, 0.5):
 #     print(i)
 
+# lamda表达式
+# lambda : True
+#
+# def add(x,y):return x+y
+#
+# lambda x,y:x+y
+
+# 函数
+# a = [1, 2, 3, 4, 5, 6, 7]
+# b = list(filter(lambda x:x>2,a))
+# print(b)
+
+# a = [1, 2, 3]
+# b = [2, 3, 4]
+# c = list(map(lambda x, y: x+y, a, b))
+# print(c)
+
+
+# from functools import reduce
+# a = reduce(lambda x, y: x+y, [1, 2, 3], 1)
+# print(a)
+
+
+# for i in zip((1, 2, 3), (4, 5, 6)):
+#     print(i)
+
+
+# dicta = {'a': 'aa', 'b': 'bb'}
+# dictb = zip(dicta.values(),dicta.keys())
+# print(dict(dictb))
+
+
+# 闭包
+# def sum(a):
+#     def add(b):
+#         return a + b
+#
+#     return add
+#
+# num1 = sum(2)
+# print(num1(1))
+
+
+def counter():
+    cnt = [0]
+    def add_one():
+        cnt[0] += 1
+        return cnt[0]
+
+    return add_one
+
+num1 =  counter(5)
+
+print(num1())
+print(num1())
+print(num1())
+print(num1())
