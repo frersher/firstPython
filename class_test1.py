c9b3988dfea93bbf4181b1d29cd50aa9c2a93fda
chenbang <1125272509@qq.com>
2022-03-16 23:40:08 +0800

    类与实例

---
+++ class_test1.py
@@ -0,0 +1,23 @@
+# 类与实例
+# user1 = {"name": "tom", "hp": 100}
+# user2 = {"name": "jerry", "hp": 80}
+#
+#
+# def print_role(rolename):
+#     print("name is %s,hp is %s" % (rolename['name'], rolename['hp']))
+#
+# print_role(user1)
+
+class Player():  # 定义一个类
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+    def print_role(self):
+        print("name is %s,hp is %s" %(self.name, self.hp))
+
+
+user1 = Player('tom', 100)  # 类的实例化
+user2 = Player('jerry', 90)
+user1.print_role()
+user2.print_role()
--- mymode.py
+++ mymode.py
@@ -1,2 +1,3 @@
 def print_me():
     print("me")
+
