0bc4ca1e1528d661f648e8b4370f075daad05bea
chenbang <1125272509@qq.com>
2022-03-17 23:19:04 +0800

    类的继承

--- class_test1.py
+++ class_test1.py
@@ -22,19 +22,41 @@ class Player():  # 定义一个类
         self.__name = newname
 
 
+# 36 类的继承
 class Monster():
     '定义怪物的类'
+    def __init__(self, hp = 100):
+        self.hp = hp
+    def run(self):
+        print('移动到某个位置')
+
+class Animals(Monster):
+    '普通的怪物'
+    def __init__(self, hp=10):
+        self.hp = hp
+
+
+class Boss(Monster):
+    'Boss类怪物'
     pass
 
+a1 = Monster(200)
+print(a1.hp)
+print(a1.run())
 
-user1 = Player('tom', 100, 'war')  # 类的实例化
-user2 = Player('jerry', 90, 'master')
-user1.print_role()
-user2.print_role()
+a2 = Animals()
+print(a2.hp)
+print(a2.run())
 
-# 通过修改名称方法
-user1.updateName('zhangs')
-user1.print_role()
-# 如果变量用__修饰则为私有的
-user1.name = 'lis'
-user1.print_role()
+
+# user1 = Player('tom', 100, 'war')  # 类的实例化
+# user2 = Player('jerry', 90, 'master')
+# user1.print_role()
+# user2.print_role()
+#
+# # 通过修改名称方法
+# user1.updateName('zhangs')
+# user1.print_role()
+# # 如果变量用__修饰则为私有的
+# user1.name = 'lis'
+# user1.print_role()
